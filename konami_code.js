4e89ce6105ebd3edd450e89ea34bda932feb5c49
Sean Christopher Febo <scf@techie.com>
2018-07-21 02:39:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-function init(e) {
+function init() {
   let index = 0;
   document.body.addEventListener('keydown', (e) => {     const key = parseInt(e.which || e.detail || e.location); 
   if (key === code[index]) {

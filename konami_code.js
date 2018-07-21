5f064359582e981c8ef7c9e60dc5d778421cd93d
Sean Christopher Febo <scf@techie.com>
2018-07-21 02:38:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-let index = 0; 
 
 function init(e) {
+  let index = 0;
   const key = parseInt(e.detail || e.which); 
   if (key === code[index]) {
     index++; 
